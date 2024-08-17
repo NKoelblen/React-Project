ec99ec8bbe83152c3753c7b4a4f0d95fffd1cbc1
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-08-17 10:02:12 +0200

    Article

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,3 +1,26 @@
+import { Alert } from '../components/Alert';
+import { Spinner } from '../components/Spinner';
+import { useFetch } from '../hooks/useFetch';
+import { useHashNavigation } from '../hooks/useHashNavigation';
+
 export function Single() {
-	return <h1>Article : </h1>;
+	const { page } = useHashNavigation();
+	const id = page.split('-')[1];
+	const { loading, data, error } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+	if (loading) {
+		return <Spinner />;
+	}
+	if (error) {
+		return <Alert type="danger">{error.toString()}</Alert>;
+	}
+	return (
+		<>
+			<h1>{data.title}</h1>
+			<img
+				src={`https://picsum.photos/id/${data.id}/800/600`}
+				alt=""
+			/>
+			<p>{data.body}</p>
+		</>
+	);
 }
